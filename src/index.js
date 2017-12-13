eadd956adf1a57d0c88f57d2fc906efe4e038ef9
VladVes <veselov.workflow@gmail.com>
2017-12-13 11:23:28 +0700

    add Rollbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import Koa from 'koa';
+import Rollbar from 'rollbar';
 import Router from 'koa-router';
 
 import addRoutes from './routes'
 
 export default () => {
   const app = new Koa();
+  const rollbar = new Rollbar('d127b6e52cdd4ebcaea93d684c756d7e');
+
+  app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      rollbar.error(err, ctx.request);
+    }
+  });
 
   const router = new Router();
   addRoutes(router);
