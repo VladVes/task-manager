dd02797ebfdc3755d6d57bf65b8489bc4712996b
VladVes <vccs@inbox.ru>
2017-12-15 08:01:39 +0700

    add koa-session

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import Rollbar from 'rollbar';
 import Router from 'koa-router';
+import session from 'koa-generic-session';
 
 import addRoutes from './routes'
 
@@ -8,6 +9,7 @@ export default () => {
   const app = new Koa();
   const rollbar = new Rollbar('d127b6e52cdd4ebcaea93d684c756d7e');
 
+  app.use(session(app));
   app.use(async (ctx, next) => {
     try {
       rollbar.log('Starting application...');
