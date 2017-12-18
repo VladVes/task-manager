25506547aef538595c750d4f622b5a6891a13905
VladVes <veselov.workflow@gmail.com>
2017-12-18 19:52:04 +0700

    fix route

--- src/routes/hellowfromdb.js
+++ src/routes/hellowfromdb.js
@@ -1,11 +1,11 @@
 import getLogger from '../lib/log';
-import hellowfromdb from '../models';
+import { testModel } from '../models';
 
 const log = getLogger('HTTP:');
 export default (router) => {
   router.get('hellowfromdb', '/hellowfromdb', async (ctx) => {
     log(`GET ${ctx.request.href}`);
-    const hellow = await hellowfromdb.findAll();
+    const hellow = await testModel.findAll();
     ctx.render('testView/hellowfromdb', { hellow });
   });
 };
