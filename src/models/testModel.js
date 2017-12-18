9e713e22241a630d1a586e42e0ba3da51254feb5
VladVes <veselov.workflow@gmail.com>
2017-12-18 19:43:44 +0700

    fix testModel

--- src/models/testModel.js
+++ src/models/testModel.js
@@ -1,6 +1,10 @@
 export default (sequelize, DataTypes) => {
   const testModel = sequelize.define('testModel', {
-    id: DataTypes.INTEGER,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     name: DataTypes.TEXT,
   });
   return testModel;
