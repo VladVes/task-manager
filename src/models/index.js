6b28e20fa38110514ca4323360c9a84808e25aa9
VladVes <vccs@inbox.ru>
2017-12-18 15:42:13 +0700

    add testModel

---
+++ src/models/index.js
@@ -0,0 +1,18 @@
+var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var config = require('__dirname' + 'config/config.js')[env];
+import getLogger from '../lib/log';
+
+const log = getLogger('ORM');
+
+if (config.use_env_var) {
+  var sequelize = new Sequelize(process.env[config.use_env_var]);
+  log(`connecting to DB by url`);
+} else {
+  var sequelize = new Sequlize(config.database, config.username, config.password, config);
+  log(`connecting to DB by config file`);
+}
+
+const testMode = sequelize.import('./testModel');
+
+export { testModel };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import welcome from './welcome';
+import hellowfromdb from './hellowfromdb';
 
-const controllers = [welcome];
+const controllers = [welcome, hellowfromdb];
 
 export default (router) => controllers.forEach(f => f(router));
