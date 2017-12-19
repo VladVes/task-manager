33f4d45fd312b4a1bdfa334f7404ce7b10c5518c
VladVes <vccs@inbox.ru>
2017-12-19 13:46:05 +0700

    add seeds

---
+++ seeds/20171219031619-demo-user.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'doe@mail.com',
+        passwordDigest: '123dddeeefff'
+      }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
