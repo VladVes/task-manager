1c20cae976b7adbd32e732f6b312b1fc962a31ce
VladVes <veselov.workflow@gmail.com>
2017-12-19 21:05:04 +0700

    update user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,5 +13,16 @@ export default (router) => {
   .get('newUser', '/users/new', (ctx) => {
       const user = User.build();
       ctx.render('users/new', { f: buildFormObj(user) });
-  });
+  })
+  .post('users', '/users', async (ctx) => {
+      const form = ctx.request.body.form;
+      const user = User.build(form);
+      try {
+        await user.save();
+        ctx.flash.set('User has been created');
+        ctx.redirect(router.url('root'));
+      } catch (e) {
+        ctx.render('users/new', { f: buildFormObj(user, e) });
+      }
+});
 };
