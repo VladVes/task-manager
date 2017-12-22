ee1324e22dcb401019261083618968e808b67528
VladVes <vccs@inbox.ru>
2017-12-22 10:20:25 +0700

    add delet profile route

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,6 @@ import getLogger from '../lib/log';
 import buildFormObj from '../lib/formObjectBuilder';
 import { User } from '../models';
 
-
 const log = getLogger('HTTP:');
 export default (router) => {
   router.get('users', '/users', async (ctx) => {
@@ -11,55 +10,56 @@ export default (router) => {
     ctx.render('users', { users });
   })
   .get('newUser', '/users/new', (ctx) => {
-      const user = User.build();
-      ctx.render('users/new', { f: buildFormObj(user) });
+    const user = User.build();
+    ctx.render('users/new', { f: buildFormObj(user) });
   })
   .get('profile', '/user/profile', async (ctx) => {
-      if (ctx.state.isSignedIn()) {
-        const id = ctx.session.userId;
-        const user = await(User.findById(id));
-        ctx.render('users/profile', { f: buildFormObj(user) });
-      } else {
-        ctx.flash.set('You should sing IN or sign UP first.');
-        ctx.redirect(router.url('root'));
-      }
+    if (ctx.state.isSignedIn()) {
+      const id = ctx.session.userId;
+      const user = await(User.findById(id));
+      ctx.render('users/profile', { f: buildFormObj(user) });
+    } else {
+      ctx.flash.set('You should sing IN or sign UP first.');
+      ctx.redirect(router.url('root'));
+    }
   })
-  .patch('profile', '/user/profile', async (ctx) => {
-      if (ctx.state.isSignedIn()) {
-        const data = ctx.request.body.form;
-        const user = await(User.findById(ctx.session.userId));
-        try {
-          await(user.update(data));
-          ctx.flash.set('Profile saved successfully.');
-          ctx.redirect(router.url('root'));
-        } catch (e) {
-            ctx.render('users/profile', { f: buildFormObj(user, e) });
-        }
-      } else {
-        ctx.flash.set('You should sing IN or sign UP first.');
+  .patch('updateProfile', '/user/profile', async (ctx) => {
+    if (ctx.state.isSignedIn()) {
+      const data = ctx.request.body.form;
+      const user = await(User.findById(ctx.session.userId));
+      try {
+        await(user.update(data));
+        ctx.flash.set('Profile saved successfully.');
         ctx.redirect(router.url('root'));
+      } catch (e) {
+          ctx.render('users/profile', { f: buildFormObj(user, e) });
       }
+    } else {
+      ctx.flash.set('You should sing IN or sign UP first.');
+      ctx.redirect(router.url('root'));
+    }
   })
   .post('users', '/users', async (ctx) => {
-      const form = ctx.request.body.form;
-      const user = User.build(form);
-      try {
-        await user.save();
-        ctx.flash.set('User has been created');
-        ctx.redirect(router.url('root'));
-      } catch (e) {
-        ctx.render('users/new', { f: buildFormObj(user, e) });
-      }
+    const form = ctx.request.body.form;
+    const user = User.build(form);
+    try {
+      await user.save();
+      ctx.flash.set('User has been created');
+      ctx.redirect(router.url('root'));
+    } catch (e) {
+      ctx.render('users/new', { f: buildFormObj(user, e) });
+    }
   })
-  .delete('users', '/users/delete', async (ctx) => {
-      const form = ctx.request.body.form;
-      const user = User.build(form);
-      try {
-        await user.save();
-        ctx.flash.set('User has been created');
-        ctx.redirect(router.url('root'));
-      } catch (e) {
-        ctx.render('users/new', { f: buildFormObj(user, e) });
-      }
+  .delete('deleteUser', '/user/delete', async (ctx) => {
+    if (ctx.state.isSignedIn()) {
+      const user = await(User.findById(ctx.session.userId));
+      user.destroy();
+      ctx.session = {};
+      ctx.flash.set('Accaunt has been deleted');
+      ctx.redirect(router.url('root'));
+    } else {
+      ctx.flash.set('You should sing IN or sign UP first.');
+      ctx.redirect(router.url('root'));
+    }
   });
 };
