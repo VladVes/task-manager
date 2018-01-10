90ec7d5e34bc1dc7bfb220f10823bbb5831acbde
VladVes <vccs@inbox.ru>
2018-01-10 13:52:36 +0700

    add new default status

--- seeds/20180104122906-task-status.js
+++ seeds/20180104122906-task-status.js
@@ -3,10 +3,11 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('TaskStatuses', [
+      { name: 'undefined (you should create new status)' },
       { name: 'new' },
       { name: 'in work' },
       { name: 'on testing' },
-      { name: 'finished' }
+      { name: 'finished' },
     ], {});
   },
 
