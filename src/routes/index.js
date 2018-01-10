b8740e6a1e3040a40da1859d1fe3817258e91091
VladVes <vccs@inbox.ru>
2018-01-10 10:42:45 +0700

    add statuses router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,8 @@ import welcome from './welcome';
 import sessions from './sessions';
 import users from './users';
 import tasks from './tasks';
+import statuses from './statuses';
 
-const controllers = [welcome, users, sessions, tasks];
+const controllers = [welcome, users, sessions, tasks, statuses];
 
 export default router => controllers.forEach(f => f(router));
