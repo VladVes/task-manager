d38101b16f012c3862fa0acc9e738224b1ed606f
VladVes <vccs@inbox.ru>
2018-01-10 11:55:52 +0700

    add new route handlers

--- src/routes/statuses.js
+++ src/routes/statuses.js
@@ -7,12 +7,48 @@ export default (router) => {
     const statuses = await TaskStatus.findAll();
     ctx.render('statuses', { statuses });
   })
-  .delete('deleteStatus', '/status/delete/:statusId', async (ctx) => {
+  .get('status', '/status/:statusId', async (ctx) => {
     const { statusId: id } = ctx.params;
     const status = await TaskStatus.findById(id);
-    const name = status.name;
-    status.destroy();
-    ctx.flash.set(`Status "${name}" deleted`);
-    ctx.redirect(router.url('statuses'));
+    ctx.render('statuses/edit', { f: buildFormObj(status) });
+  })
+  .get('newStatus', '/staus/new', async (ctx) => {
+    if (ctx.state.isSignedIn()) {
+      const status = Task.build();
+      ctx.render('statuses/new', { f: buildFormObj(status) });
+    } else {
+      ctx.flash.set('You should sing IN or sign UP first.');
+      ctx.redirect(router.url('root'));
+    }
+  })
+  .patch('editStatus', '/status/edit/:statusId', async (ctx) => {
+    if (ctx.state.isSignedIn()) {
+      const { statusId: id } = ctx.params;
+      const data = ctx.request.body.form;
+      const status = await TaskStatus.findById(id);
+      try {
+        await status.update(data);
+        ctx.flash.set('Status updated.');
+        ctx.redirect(router.url('statuses'));
+      } catch (e) {
+        ctx.render('statuses/edit', { f: buildFormObj(status, e) });
+      }
+    } else {
+      ctx.flash.set('You should sing IN or sign UP first.');
+      ctx.redirect(router.url('root'));
+    }
+  })
+  .delete('deleteStatus', '/status/delete/:statusId', async (ctx) => {
+    if (ctx.state.isSignedIn()) {
+      const { statusId: id } = ctx.params;
+      const status = await TaskStatus.findById(id);
+      const name = status.name;
+      status.destroy();
+      ctx.flash.set(`Status "${name}" deleted`);
+      ctx.redirect(router.url('statuses'));
+    } else {
+      ctx.flash.set('You should sing IN or sign UP first.');
+      ctx.redirect(router.url('root'));
+    }
   });
 };
