d5e2d9555c3acac4d6c7785c076d796679286804
VladVes <veselov.workflow@gmail.com>
2018-01-10 23:15:48 +0700

    fix tags

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,6 +1,8 @@
 import getLogger from '../lib/log';
 import buildFormObj from '../lib/formObjectBuilder';
-import { Task, Tag, User, Creator, TaskStatus } from '../models';
+import { Task, Tag, User, Creator, TaskStatus, Sequelize } from '../models';
+
+const Op = Sequelize.Op;
 
 export default (router) => {
   router.get('tasks', '/tasks', async (ctx) => {
@@ -28,17 +30,26 @@ export default (router) => {
   .post('saveTask', '/tasks', async (ctx) => {
     if (ctx.state.isSignedIn()) {
       const data = ctx.request.body.form;
-      data.Tags = data.tags.split(',').map(tag => ({ name: tag }));
-      data.creator = ctx.session.userId;
+      const tagNames = data.tags.split(',').map(tag => tag.trim());
+      const existingTags = await Tag.findAll({
+        where: { name: { [Op.or]: tagNames } },
+      });
+      const existingTagNames = existingTags.map(tag => tag.name);
+      data.Tags = tagNames.filter(name => !existingTagNames.includes(name))
+        .map(name => ({ name }));
 
+      data.creator = ctx.session.userId;
       const newTask = Task.build(data, { include: [Tag] });
+      newTask.addTag(existingTags);
       try {
         await newTask.save();
         ctx.flash.set('New task has been created successfully');
         ctx.redirect(router.url('tasks'));
       } catch (e) {
+        const statuses = await TaskStatus.findAll();
         const users = await User.findAll();
-        ctx.render('tasks/new', { f: buildFormObj(newTask, e), users });
+        console.log("!!!!!!!!!!!!!!!!!!!!ERROR: ", e);
+        ctx.render('tasks/new', { f: buildFormObj(newTask, e), users, statuses });
       }
     } else {
       ctx.flash.set('You should sing IN or sign UP first.');
@@ -50,21 +61,28 @@ export default (router) => {
     const task = await Task.findById(id, {
       include: [
         { model: User, paranoid: false },
-        Creator,
+        { association: Creator, paranoid: false },
         { model: TaskStatus, paranoid: false },
         Tag,
       ],
     });
     task.tags = task.Tags.map(tag => tag.name).join(', ');
     const users = await User.findAll({ paranoid: false });
-    const statuses = await TaskStatus.findAll({ paranoid: false });
+    const statuses = await TaskStatus.findAll();
     ctx.render('tasks/edit', { f: buildFormObj(task), users, statuses });
   })
   .patch('editTask', '/tasks/edit/:taskId', async (ctx) => {
     if (ctx.state.isSignedIn()) {
       const { taskId: id } = ctx.params;
       const data = ctx.request.body.form;
-      const task = await Task.findById(id);
+      const task = await Task.findById(id, {
+        include: [
+          { model: User, paranoid: false },
+          { association: Creator, paranoid: false },
+          { model: TaskStatus, paranoid: false },
+          Tag,
+        ],
+      });
     try {
         await task.update(data);
         ctx.flash.set('Task updated successfully.');
