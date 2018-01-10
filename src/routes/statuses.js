8b6807a56700b5ee95aaf5c36f07153487d93897
VladVes <vccs@inbox.ru>
2018-01-10 09:32:58 +0700

    add delete route

--- src/routes/statuses.js
+++ src/routes/statuses.js
@@ -6,5 +6,13 @@ export default (router) => {
   router.get('statuses', '/statuses', async (ctx) => {
     const statuses = await TaskStatus.findAll();
     ctx.render('statuses', { statuses });
+  })
+  .delete('deleteStatus', '/status/delete/:statusId', async (ctx) => {
+    const { statusId: id } = ctx.params;
+    const status = await TaskStatus.findById(id);
+    const name = status.name;
+    status.destroy();
+    ctx.flash.set(`Status "${name}" deleted`);
+    ctx.redirect(router.url('statuses'));
   });
 };
