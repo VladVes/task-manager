ae668a0e428f352dbedb7deb05fd3aea6ffb2c37
VladVes <veselov.workflow@gmail.com>
2018-01-13 21:47:53 +0700

    add migrations up for test env

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,15 @@
-var fs = require('fs');
-var path = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env = process.env.NODE_ENV || 'development';
-var config = require('__dirname' + '/../../config/config.js')[env];
-var db = {};
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 import getLogger from '../lib/log';
+import dbInit from '../../migrations';
+
+dotenv.config();
+const basename  = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+let config = require('__dirname' + '/../../config/config.js')[env];
+let db = {};
 
 const log = getLogger('ORM');
 
@@ -15,6 +19,10 @@ if (config.use_env_variable) {
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
   log(`connecting to DB by using config file`);
+}
+
+if (env == 'test') {
+  dbInit(sequelize.queryInterface, Sequelize);
 }
 
 fs
